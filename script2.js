7f5244e224f53b09b6cef0f63d1a52a09d6d7094
Gokulcasper <ronitcasper215@gmail.com>
2022-06-23 15:51:09 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,27 @@
+const gamesListEl = document.querySelector(".input-list");
+
+// const list = document.querySelectorAll("li");
+
+// // querySelectorAll:  NodeList
+
+// console.log(list, list.length);
+
+// // .children : HTMLCollection
+
+// console.log(gamesListEl.children, gamesListEl.children.length);
+
+// // childNodes :
+
+// console.log(gamesListEl.childNodes);
+
+// firstElementChild
+
+console.log(gamesListEl.firstChild);
+
+console.log(gamesListEl.firstElementChild.innerText);
+
+// lastElementChild
+
+console.log(gamesListEl.lastChild);
+
+console.log(gamesListEl.lastElementChild.innerText);
